9e312109c2d7b9bf468c758552dc81a888d7e851
Owen <97751860+owen-laney@users.noreply.github.com>
2025-02-08 22:12:26 -0500

    Add add more info page under device/add-info/[id]

--- mobile/app/device/[id].tsx
+++ mobile/app/device/[id].tsx
@@ -1,6 +1,6 @@
 import { BLEconnection } from '@/components/BLEconnection';
-import { Stack, useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Stack, useLocalSearchParams, Link } from 'expo-router';
+import { Text, View, ScrollView } from 'react-native';
 
 export default function DevicePage() {
   // Access the dynamic route parameter 'id'
@@ -8,11 +8,12 @@ export default function DevicePage() {
 
   // Render the component based on the 'id'
   return (
-    <View>
+    <ScrollView>
+      <Link href={'./add-info/' + id}><Text className='text-2xl font-medium color-blue-600'>Add more info</Text></Link>
       <Stack.Screen options={{ headerBackTitle: 'Explore' }} />
       <Text>Device ID: {id}</Text>
       <BLEconnection />
       {/* You can use the 'id' to fetch device data or render specific content */}
-    </View>
+    </ScrollView>
   );
 }
