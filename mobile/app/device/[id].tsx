528517b7b87d0557e093cc6eabe839e0a544f634
Tytorb <Tytorb@gmail.com>
2025-02-09 07:46:58 -0500

    finish

--- mobile/app/device/[id].tsx
+++ mobile/app/device/[id].tsx
@@ -8,43 +8,15 @@ export default function DevicePage() {
   // Access the dynamic route parameter 'id'
   const { user } = useAuth0();
   const { id } = useLocalSearchParams(); // 'id' corresponds to the dynamic part of the URL
-  const [data, setData] = useState<any>([]);
-
-  const getMovies = async () => {
-    try {
-      const response = await fetch(
-        'https://pink-petite-leech-526.mypinata.cloud/ipfs/bafkreiekl5e2zykhlkuuplczyf4r67uq7ddkrap6c4gttajl2zbrfrvx7m',
-        {
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${process.env.JWT}`,
-          },
-        }
-      );
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  useEffect(() => {
-    console.error(data, 'here');
-  }, [data]);
 
   // Render the component based on the 'id'
   return (
-    <View className='pt-48 bg-white justify-center items-center flex-col'>
+    <View className="pt-48 bg-white justify-center items-center flex-col">
       <View>
-      <Stack.Screen options={{ headerBackTitle: 'Explore' }} />
-      <Text className='text-md font-medium p-0'>Device ID: {id}</Text>
-      <BLEconnection />
-      {/* You can use the 'id' to fetch device data or render specific content */}
+        <Stack.Screen options={{ headerBackTitle: 'Explore' }} />
+        <Text className="text-md font-medium p-0">Device ID: {id}</Text>
+        <BLEconnection />
+        {/* You can use the 'id' to fetch device data or render specific content */}
       </View>
     </View>
   );
